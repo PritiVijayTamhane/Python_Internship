a640c441d4fc4f8415c6bae0758fdf71b7ebb092
PritiVijayTamhane <tamhanepriti1@gmail.com>
2022-07-28 21:48:25 +0530

    Delete Python_Prgrm-1.py

--- Day_3/Python_Prgrm-1.py
+++
@@ -1,9 +0,0 @@
-#My first program in python
-#And this is a single line comment
-''' ___ This 
-            Is
-              A
-                Multiline
-                   Comment  ___ '''
-                   
-print("Happy Thoughts ...!!")
