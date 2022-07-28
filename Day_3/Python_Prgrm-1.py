9ca76ae90fdcf0ade939bdaf3d3c79fbbbc972a3
PritiVijayTamhane <tamhanepriti1@gmail.com>
2022-07-28 21:53:08 +0530

    Adding Single line and multiline comments

---
+++ Day_3/Python_Prgrm-1.py
@@ -0,0 +1,14 @@
+print("Happy Thoughts ...!!")
+
+#Adding comments
+#This is a single line comment
+
+''' This is a multiline comment
+    Line_1
+    Line_2
+    Line_3
+    .
+    .
+    .
+    Line_n
+    
